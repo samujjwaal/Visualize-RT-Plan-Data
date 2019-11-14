12ab58a6eb1ccfcbc8ea9ff6258dcb14e26b8e6b
Nipu <nafiulnipu.2017@gmail.com>
2019-11-14 00:10:53 -0600

    numerical distribution completed with boxplots..

--- js/test.js
+++ js/test.js
@@ -1,34 +1,12 @@
-var scenes = [],
-    rendered;
-
-var selectedPatient = 1;
-var syncCameras = true,
-    syncCamerasInterval,
-    detailOnRotate = true;
-
-var raycaster;
-var mouse = new THREE.Vector2(-500, -500);
-var mouseNorm = new THREE.Vector2(-500, -500),
-    INTERSECTED = null,
-    nodeHover = null;
-
-var camraDistZ = 500;
-
-//data
-var organs, oAtlas, links;
-var organModels = new THREE.GROUP();
-
-var partitions = ["Oral Cavity & Jaw", "Throat", "Salivary Glands", "Eyes", "Brainstem & Spinal Cord", "Other"];
-
-var canvas = document.getElementById("c");
-
-var data;
-var meshes;
-var files = ["data/organAtlas.json", "data/patient_dataset.json"];
-
-files.forEach(function(url){
-    oAtlas = values[0][0];
-    data = Data(values[1], oAtlas);
-    meshes = loadOrganMeshes();
+d3.json("data/patient_dataset.json").then(function(patients) {
+    var tumor_volume = getTumorVolume(patients);
+    console.log(tumor_volume);
 });
-
+function getTumorVolume(data){
+    var volume = [];
+    var count ;
+    for (count = 0; count < data.length ; count ++){
+        volume[count] = data[count].tumorVolume;
+    }
+    return volume;
+}
